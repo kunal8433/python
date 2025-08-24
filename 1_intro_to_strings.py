aa0ad19e808b636715c3cbc2ad1034f5f32f5aad
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-08-24 16:29:06 +0530

    PYTHON CHAPTER_3
    CHAPTER _3

---
+++ 1_intro_to_strings.py
@@ -0,0 +1,6 @@
+
+name ="kunal"
+
+nameshort = name[0:3] #start from index 0 all the way till 3 excluding)
+
+print(nameshort)
---
+++ 3_str_function.py
@@ -0,0 +1,14 @@
+name = "kunal"
+
+print (len(name))
+
+print(name.endswith("al"))
+
+print (name.startswith("ku"))
+
+print(name.capitalize())
+print (len("kunal"))
+
+
+
+
