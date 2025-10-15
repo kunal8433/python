3a13a5ad019eeab2b40c74c6eb9fcc21521b0558
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-10-15 19:36:48 +0530

    CHAT GPT.PY

--- chathpt.py
+++ chathpt.py
@@ -1,24 +1,40 @@
 import random
-
 '''
-1 for snake
-0 for gun
+1 for snake 
+0 for gun 
 -1 for water
 '''
+computer = random.choice([1,0,-1])
+print("Welcom to my first game {gun,water,snake}")
+print("1:snake , 0:gun , -1:water")
+youstr = (input("Enter your choice :"))
+yourDict = {"s":1,"w":-1,"g":0}
+computerDict = {1:"snake",0:"gun",-1:"water"}
 
-computer = random.choice([-1, 0, 1])
-youstr = input("Enter your choice (s for snake, g for gun, w for water): ")
+you = yourDict[youstr]
 
-youdict = {"s": 1, "w": -1, "g": 0}
-reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+print(f"you chose {computerDict[you]}\n computer chose{computerDict[computer]}")
+if(computer == you):
+    print("match draw")
 
-you = youdict[youstr]
-
-print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
-
-if computer == you:
-    print("Match draw")
-elif (you == 1 and computer == -1) or (you == 0 and computer == 1) or (you == -1 and computer == 0):
-    print("YOU WIN!")
 else:
-    print("YOU LOSS!")
+    if(computer == -1 and you == 1):
+        print("YOU WIN!")
+
+    elif(computer == -1 and you == 0):
+        print("YOU LOSS!")
+
+    elif(computer == 1 and you == -1):
+        print("YOU LOSS")
+
+    elif(computer == 1 and you == 0):
+        print("YOU WIN!")
+
+    elif(computer == 0 and you == -1):
+        print("YOU WIN!")
+
+    elif(computer == 0 and you == 1):
+        print("YOU LOSS!")
+    
+    else:
+        print("something went wrong")
