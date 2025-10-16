f50b444c7c1442c8e87f463b11a086c6f204874e
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-10-16 22:04:47 +0530

    CH.10 RS 2

---
+++ 02_ch10.py
@@ -0,0 +1,9 @@
+class student:
+    def __init__(self,name,rollno,school):
+        self.name = name
+        self.rollno = rollno
+        self.school = school
+S1 = student("Kunal", 17,"BIT GLOBLE")
+print("First student name:",S1.name)
+print("Its rollno:",S1.rollno)
+print("School name:",S1.school)
