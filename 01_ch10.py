06113f77dddd6962b904a24caff37c1aeaf200a1
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-10-16 22:04:19 +0530

    CH.10 RS
    CH.10 RS

---
+++ 01_ch10.py
@@ -0,0 +1,16 @@
+class Student:
+    School = "BIT GLOBLE" # class attribute
+    def __init__(Self,name,rollno):
+        Self.name = name
+        Self.rollno = rollno
+
+school = "BIT GLOBLE"
+S1 = Student ("Kunal", 17, ) #instance attribute
+S2 = Student ("Lucky", 13 ) #instance attribute
+
+print("WELCOME TO BIT GLOBLE SCHOOL CLASSES STUDENT RECODES")
+print()
+
+print (("First student name:",S1.name)  ("Its rollno:",S1.rollno ),("School name:",  school))
+
+print(("Second student name:",S2.name) , ("Its rollno:",S2.rollno) ,("School name:" , school) )
