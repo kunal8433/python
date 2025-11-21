2d74a2f9b8d9ede2fb9dc69727724924bcc1576d
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-11-21 18:12:59 +0530

    revision

---
+++ 11_problem.py
@@ -0,0 +1,9 @@
+fruit1 = input("Enter your fruit 1:")
+fruit2 = input("Enter your fruit 2:")
+fruit3 = input("Enter your fruit 3:")
+fruit4 = input("Enter your fruit 4:")
+fruit5 = input("Enter your fruit 5:")
+fruit6 = input("Enter your fruit 6:")
+fruit7 = input("Enter your fruit 7:")
+my_fruit_list = [fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7]
+print("The fruit list is:", my_fruit_list)
