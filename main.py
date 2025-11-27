6425a46d9b0a14e8cf83ebbe278045fb35374bd7
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-11-27 10:54:27 +0530

    basic projects

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+if __name__ == "__main__":
+     print("Welcome to kunal robo speaker 1.1")
+     print()
+     x = input("Enter your command:")
+     
+     command = f" {x}"
+     os.system(command)
