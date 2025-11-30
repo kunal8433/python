6a5a03bf5de862241dc4700ea5ced0a56df00b84
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-11-30 20:39:19 +0530

    practice

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
-import os
+import random
+'''
+1 for snake 
+2 for water 
+3 for gun 
 
-if __name__ == "__main__":
-     print("Welcome to kunal robo speaker 1.1")
-     print()
-     x = input("Enter your command:")
-     
-     command = f" {x}"
-     os.system(command)
+'''
+
+computer = random.choice([1,2,3])
+maindict = {1:"snake" , 2:"water" , 3:"gun"}
+youdict = {"s":1, "g":2 , "w":3}
+print("WELCOM TO MY GAME")
+print()
+print("1 for snake")
+print("2 for water")
+print("3 for gun")
+
+yourstr = input("Enter your choice:")
+
+you = youdict [ yourstr]
+
+
+
+print(f"You  chose {maindict[you]}/n computer chose {maindict[computer]}")
+
+if computer == you :
+    print("MATCH DRAW!")
+
+else :
+    if computer == 1 and you == 1 :
+        print("YOU WIN !")
+    elif computer == 1 and you == 2 :
+        print("YOU loss !")
+    elif computer == 1 and you == 3 :
+        print("YOU loss !")
+    elif computer == 3 and you == 2 :
+        print("YOU loss !")
+    elif computer == 2 and you == 3 :
+        print("YOU loss !")
+    elif computer == 1 and you == 1 :
+        print("YOU loss !")
+    elif computer == 3 and you == 2 :
+        print("YOU loss !")
+    elif computer == 2 and you == 1 :
+        print("YOU loss !")
+    else:
+        print("error")
+
