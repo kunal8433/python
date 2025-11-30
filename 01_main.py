083b4050b5ec18d5b7ccc4ce1207bb79f904c454
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2025-11-30 20:38:56 +0530

    practice

--- 01_main.py
+++ 01_main.py
@@ -1,15 +1,41 @@
-import speech_recognition as sr
-import webbrowser
+import random
 
-import pyttsx3
+'''
+1 for gun
+2 for water
+3 for snake 
 
-recognizer = sr.Recognizer()
-engine = pyttsx3.init()
+'''
 
-def speek(text):
-    engine.say(text)
-    engine.runAndWait()
+computer = random.choice([1,2,3])
+yourstr = input("Enter your choice :")
+yourdict = { "g":1 , "w":2 , "s" :3 }
+maindict= {1:"gun",2:"w",3:"snake"}
 
-if __name__ == "main__":
-    speek("Hello, how can I assist you today?")
+you = yourdict [yourstr]
+
+print(f"your choice {maindict[you]} |and| computer choice {maindict[computer]} ")
+
+if computer == you :
+    print("MATCH DRAW!")
+
+else :
+    if computer == 1 and you == 1 :
+        print("YOU WIN !")
+    elif computer == 1 and you == 2 :
+        print("YOU loss !")
+    elif computer == 1 and you == 3 :
+        print("YOU loss !")
+    elif computer == 3 and you == 2 :
+        print("YOU loss !")
+    elif computer == 2 and you == 3 :
+        print("YOU loss !")
+    elif computer == 1 and you == 1 :
+        print("YOU loss !")
+    elif computer == 3 and you == 2 :
+        print("YOU loss !")
+    elif computer == 2 and you == 1 :
+        print("YOU loss !")
+    else:
+        print("error")
 
