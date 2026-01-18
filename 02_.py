1367e6b22519bc88e26f36f93caf4ac10019e398
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-18 18:39:04 +0530

    Add files via upload

--- 02_.py
+++ 02_.py
@@ -1,5 +1,27 @@
-r = 5 
-for i in range (1, r+1):
-    for j in range (1, i+1):
-        print("*", end = " ")
-    print()
+print("Word Guessing Game 🧠")
+
+word = "code"
+guessed = ""
+tries = 5
+
+while tries > 0:
+    letter = input("Guess a letter: ")
+
+    if letter in word:
+        guessed += letter
+        print("Good guess 👍")
+    else:
+        tries -= 1
+        print("Wrong guess ❌, Tries left:", tries)
+
+    done = True
+    for ch in word:
+        if ch not in guessed:
+            done = False
+
+    if done:
+        print("You guessed the word 🎉:", word)
+        break
+
+if tries == 0:
+    print("Game Over 😢, Word was:", word)
