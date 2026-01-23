133d8d32c6622d53c49f2458062ce3f056c13dc9
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-01-23 22:22:49 +0530

    basic questions

--- 01_.py
+++ 01_.py
@@ -1,4 +1,15 @@
-import pandas as pd
-data = {"name": ["kunalmalik", "lucky malik"] , "marks":[98,99]}
-ds = pd.Series(data)
-print(ds)
+lucky = int(input("enter your number for check:"))
+even = lucky //2
+odd = lucky // 3
+
+if lucky == even *2:
+        print("your number is even")
+        
+        
+elif lucky == odd *3:
+         print("your number is odd")
+         
+else:
+         print("your number is invalid")
+         
+
