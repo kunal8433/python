644a3f9c3fdd3ccc24eba91bbd983d74905dfd29
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-01 23:50:46 +0530

    matplotlib

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-a = int(input("Enter your no:"))
-print(eval(a))
+import matplotlib.pyplot as plt
+x = [2023, 2024, 2025, 2026]
+y = [10, 15, 5, 40]
+plt.plot(x, y)
+plt.xlabel('Year')
+plt.ylabel('Value')
+plt.title('Yearly Values')
+plt.show()
