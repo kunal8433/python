027605a7bd524c3b8c797e04966a907f837c1e64
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-04 21:59:58 +0530

    basic Q/P.S

--- 04.py
+++ 04.py
@@ -1,31 +1,4 @@
-from turtle import *
-import colorsys
-bgcolor('yellow')
-tracer (500)
-
-def draw():
-
-    h= 0
-    for i in range(100):
-        c=colorsys.hsv_to_rgb(h,1,1)
-        h +=0.5
-        up()
-        goto(0,0)
-        down()
-        color('red')
-        fillcolor(c)
-        begin_fill()
-        rt(98)
-        circle(i,12)
-        fd(290)
-        fd(i)
-        lt(29)
-        for j in range(129):
-            fd(i)
-            circle(i,50000,steps=8)
-            end_fill()
-draw()
-done()            
-            
-        
-        
+user  = int(input("Enter your table:"))
+for i in range(1, 11):
+    print(f"{user} x {i} = {user * i}")
+# This code takes an integer input from the user and prints the multiplication table for that number from 1 to 10.
