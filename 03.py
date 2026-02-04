5ec28a98eba4056c3926a3dbca7713f8f75c23d8
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-04 21:59:38 +0530

    basic Q/P.S

--- 03.py
+++ 03.py
@@ -1,14 +1,6 @@
-from turtle import*
-speed (0)
-bgcolor("black")
-colors = ["orange","green"]
-hideturtle()
-for i in range(122):
-    goto(0, 0)
-    color(colors[i % 2])
-    forward (130)
-    left(3)
-    circle(40)
-    forward(130)
-    right(180)
-done()
+user = int(input("Enter a number: "))
+if user % 2 == 0:
+
+    print("even")
+else:
+    print("odd")
