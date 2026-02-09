529b840c9f2a18cff568499d6e836c1d95211ae9
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-09 23:39:38 +0530

    basic problem

--- 02_.py
+++ 02_.py
@@ -1,9 +1,6 @@
-no_1 = 65
-no_2 = 23
-no_3 = 89
-if no_1 > no_2 and no_1 > no_3:
-    print("no_1 is greater")
-elif no_2 > no_1 and no_2 > no_3:
-    print("no_2 is greater")
-else:
-    print("no_3 is greater")
+nums = int(input("Enter your number:"))
+
+for i in range(1, nums + 1):
+    for j in range(i):
+        print("*",end = " ")
+    print()
