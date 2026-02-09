805b55055b56f98e06ecea04abbdd0578522eafd
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-09 23:39:22 +0530

    basic problem

--- 01_.py
+++ 01_.py
@@ -1,15 +1,5 @@
-lucky = int(input("enter your number for check:"))
-even = lucky //2
-odd = lucky // 3
-
-if lucky == even *2:
-        print("your number is even")
-        
-        
-elif lucky == odd *3:
-         print("your number is odd")
-         
-else:
-         print("your number is invalid")
-         
-
+nums = [12, 5, 8, 20, 3, 15]
+print(sum(nums))
+for num in nums:
+    if num % 2 == 0:
+        print(num)
