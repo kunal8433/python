4fbec6be1ff92ee3dfc262430ebdccdc327fbca0
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-10 19:51:47 +0530

    basic question

--- 06_.py
+++ 06_.py
@@ -1,9 +1,6 @@
-for i in range (2,6):
-    for j in range (i):
-        print("*", end = "")
+num = int(input("Enter a number: "))
+for i in range(1, num + 1):
+    for j in range(i):
+     print("*", end = "")
     print()
-
-for i   in range (5,0,-1):
-    for j in range (i):
-        print("*"  ,end = "")
-    print()
+          
