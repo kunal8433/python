468a8dadcb83641ef009a50926051cbf03b977e5
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-10 21:08:01 +0530

    basic question

--- 07_.py
+++ 07_.py
@@ -1,21 +1,6 @@
-while True:
-    print("\n1.Add  2.Subtract  3.Multiply  4.Divide  5.Exit")
-    choice = int(input("Enter choice: "))
+subject = input("Enter your today subject:")
+time = int(input("How many time you give [hr]:"))
+topic = input("In which topic yor study:")
 
-    if choice == 5:
-        print("Exited")
-        break
+print(f"you study : {subject} \n time is : {time} \n the topic is :{topic}")
 
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
-
-    if choice == 1:
-        print("Result:", a + b)
-    elif choice == 2:
-        print("Result:", a - b)
-    elif choice == 3:
-        print("Result:", a * b)
-    elif choice == 4:
-        print("Result:", a / b)
-    else:
-        print("Invalid choice")
