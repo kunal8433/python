1c053c246311fc91b0ffd68024decbde02c4ff5a
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-14 17:12:38 +0530

    Add files via upload

--- 03_.py
+++ 03_.py
@@ -1,9 +1,1 @@
-def is_prime(n):
-    for i in range(1,50):
-        if n % i == 0:
-            print(i)
-        else:
-            print("Not a prime number")
-number = int(input("Enter a number: "))
-is_prime(number)
-    
+print("hello world")
