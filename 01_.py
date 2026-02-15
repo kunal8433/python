e0d8268cd49f5299c4b7ed0f85fadd34b3e020f8
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-15 19:16:00 +0530

    Add files via upload

--- 01_.py
+++ 01_.py
@@ -1,5 +1,9 @@
-num = int(input("Enter your number:"))
-for i in range (1 , num+1):
-    for j in range (i):
-        print("*", end = "")
-    print()
+class programer :
+    company ="Microsoft"
+    salary = "120000"
+class employe(programer):
+    name = "kunal"
+    age = "17"
+    adress = "Meerut"
+E1 = employe
+print(E1)
