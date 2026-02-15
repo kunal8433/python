c94caf97b6535489781bdd3403d5a5581f9e499d
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-15 19:16:51 +0530

    Add files via upload

--- 02_.py
+++ 02_.py
@@ -1,6 +1,14 @@
-nums = int(input("Enter your number:"))
+import random
+n = random.choice([1,100])
+a = -1
+guess = 0 
+while(a!=n):
+    a = int(input("Enter your number:"))
+    if (a>n):
+        print("guess lower number")
+    elif (a<n):
+        print("higher number plss")
+    
+        guess+1
+print(f"you have guess the number {n} correctliy in {guess} attempt")    
 
-for i in range(1, nums + 1):
-    for j in range(i):
-        print("*",end = " ")
-    print()
