afde686a6a52ba48d06e0190a776bf7c1641e22a
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-16 18:53:57 +0530

    Add files via upload

--- 06_.py
+++ 06_.py
@@ -1,6 +1,15 @@
-num = int(input("Enter a number: "))
-for i in range(1, num + 1):
-    for j in range(i):
-     print("*", end = "")
-    print()
-          
+import random
+choice = input("Enter your choice (y/n)")
+computer = random.choice[(1,2),(3,4),(5,6),
+            (1,3),(2,4),(5,1),
+      
+          (6,2),(3,5),(4,6)]
+while True :
+
+       if choice == "y":
+
+         print("You rolled a", computer)
+       elif choice == "n":
+         print("You chose not to roll the die.")
+       else:
+         print("Invalid input. Please enter 'y' or 'n'.")
