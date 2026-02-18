b9eca015312663e9ece5bc143aba39d88e70c76d
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-18 19:21:12 +0530

    Add files via upload

--- 02_.py
+++ 02_.py
@@ -1,14 +1,3 @@
-import random
-n = random.choice([1,100])
-a = -1
-guess = 0 
-while(a!=n):
-    a = int(input("Enter your number:"))
-    if (a>n):
-        print("guess lower number")
-    elif (a<n):
-        print("higher number plss")
-    
-        guess+1
-print(f"you have guess the number {n} correctliy in {guess} attempt")    
-
+table = int(input("Enter your table no. :"))
+for i in range(1, 11):
+    print(f"{table} x {i} = {table*i}")
