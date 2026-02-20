56ba04e91c08c2baf573a038c169b5d9edf84068
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-02-20 16:30:35 +0530

    Add files via upload

--- 06_.py
+++ 06_.py
@@ -1,15 +1,7 @@
-import random
-choice = input("Enter your choice (y/n)")
-computer = random.choice[(1,2),(3,4),(5,6),
-            (1,3),(2,4),(5,1),
-      
-          (6,2),(3,5),(4,6)]
-while True :
+f = open("data.txt","w")
+f.write("Hello Python")
+f.close()
 
-       if choice == "y":
-
-         print("You rolled a", computer)
-       elif choice == "n":
-         print("You chose not to roll the die.")
-       else:
-         print("Invalid input. Please enter 'y' or 'n'.")
+f = open("data.txt","r")
+print(f.read())
+f.close()
