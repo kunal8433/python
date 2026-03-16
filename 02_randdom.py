9a8012585aae781dca20a100849b16cc84af1ad9
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-16 21:15:21 +0530

    Add files via upload

---
+++ 02_randdom.py
@@ -0,0 +1,10 @@
+import random
+questions = {
+                            "Q1. who is the PM of india.? " :"modi", 
+                            "Q2. Capital of india.": "delhi", 
+                            "Q3. How many subscriber in kunal Chaudhary channel .":"23"     
+}
+
+def kunal_malik():
+    print("hellow kunal malik")
+
