49b793272cd47f9d7ee21a18f5a3dc4e33aa3bca
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-05-01 22:20:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-import matplotlib.pyplot as plt
-x = [2023, 2024, 2025, 2026]
-y = [10, 15, 5, 40]
-plt.plot(x, y)
-plt.xlabel('Year')
-plt.ylabel('Value')
-plt.title('Yearly Values')
-plt.show()
+import random
+'''
+r for rock
+p for paper
+s fro seasor
+
+'''
+print("WELCOME TO KUNAL MALIK GAME !")
+print()
+computer = random.choice([1,2,3])
+maindict = {1:"rock",2:"paper",3:"seasor"}
+
+yourstr = input("Enter your choice r/p/s :")
+yourdict = {"r":1,"p":2,"s":3}
+
+you = yourdict[yourstr]
+
+print(f"The choice of compuer is {maindict[computer]} \n your choice is {maindict[you]}")
+
+if computer == you:
+    print("Draw")
+else:
+    if computer == 1 and you == 2 :
+        print("You Win!")
+    elif computer == 1 and you == 3:
+        print("You Loss!")
+    elif computer == 2 and you == 1 :
+        print("You Loss!")
+    elif computer == 2 and you == 3:
+        print("You Win!")
+    elif computer == 3 and you == 1 :
+        print("You Win!")
+    elif computer == 3 and you == 2:
+        print("You Loss!")
+    else :
+        print("ERROR")
+        
+
+
+
