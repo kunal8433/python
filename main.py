d33419d676f311d298171a9588d7b09afa7eb8de
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-05-02 21:39:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
-import random
-'''
-r for rock
-p for paper
-s fro seasor
-
-'''
-print("WELCOME TO KUNAL MALIK GAME !")
-print()
-computer = random.choice([1,2,3])
-maindict = {1:"rock",2:"paper",3:"seasor"}
-
-yourstr = input("Enter your choice r/p/s :")
-yourdict = {"r":1,"p":2,"s":3}
-
-you = yourdict[yourstr]
-
-print(f"The choice of compuer is {maindict[computer]} \n your choice is {maindict[you]}")
-
-if computer == you:
-    print("Draw")
-else:
-    if computer == 1 and you == 2 :
-        print("You Win!")
-    elif computer == 1 and you == 3:
-        print("You Loss!")
-    elif computer == 2 and you == 1 :
-        print("You Loss!")
-    elif computer == 2 and you == 3:
-        print("You Win!")
-    elif computer == 3 and you == 1 :
-        print("You Win!")
-    elif computer == 3 and you == 2:
-        print("You Loss!")
-    else :
-        print("ERROR")
-        
-
-
-
+while True:
+ x = int(input("Enter your number :"))
+ if x<=100:
+    print("Smaller!")
+ elif x>=100:
+    print("Grater!")
+ else:
+    print("Invalid character")
