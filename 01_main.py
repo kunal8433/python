3b4f2adf35f2bac5b141dc2c08fcce4ab162137b
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-05-03 21:36:48 +0530

    Add files via upload

--- 01_main.py
+++ 01_main.py
@@ -1,41 +1,16 @@
-import random
+import random 
+n = random.randint(1, 100)
+a =-1
+gusses = 0
+while(a!=n):
+    a = int(input("Guess the number: "))
+    if(a>n):
+        print("Lower number plss")
+        gusses +=1
+    elif(a<n):
+        print("Higher number plss")
+        guesses +=1
 
-'''
-1 for gun
-2 for water
-3 for snake 
+print(f"You have guess the number {n} correctly {gusses} attempts")
 
-'''
-
-computer = random.choice([1,2,3])
-yourstr = input("Enter your choice :")
-yourdict = { "g":1 , "w":2 , "s" :3 }
-maindict= {1:"gun",2:"w",3:"snake"}
-
-you = yourdict [yourstr]
-
-print(f"your choice {maindict[you]} |and| computer choice {maindict[computer]} ")
-
-if computer == you :
-    print("MATCH DRAW!")
-
-else :
-    if computer == 1 and you == 1 :
-        print("YOU WIN !")
-    elif computer == 1 and you == 2 :
-        print("YOU loss !")
-    elif computer == 1 and you == 3 :
-        print("YOU loss !")
-    elif computer == 3 and you == 2 :
-        print("YOU loss !")
-    elif computer == 2 and you == 3 :
-        print("YOU loss !")
-    elif computer == 1 and you == 1 :
-        print("YOU loss !")
-    elif computer == 3 and you == 2 :
-        print("YOU loss !")
-    elif computer == 2 and you == 1 :
-        print("YOU loss !")
-    else:
-        print("error")
 
