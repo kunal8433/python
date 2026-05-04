61e70f022cc5f0b340bcf3f3f6fc9eb9d519e2eb
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-05-04 21:22:09 +0530

    Add files via upload

--- 02_.py
+++ 02_.py
@@ -1,3 +1,9 @@
-table = int(input("Enter your table no. :"))
-for i in range(1, 11):
-    print(f"{table} x {i} = {table*i}")
+no_1 = 65
+no_2 = 23
+no_3 = 89
+if no_1 > no_2 and no_1 > no_3:
+    print("no_1 is greater")
+elif no_2 > no_1 and no_2 > no_3:
+    print("no_2 is greater")
+else:
+    print("no_3 is greater")
